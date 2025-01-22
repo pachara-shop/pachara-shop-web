60f88e50c18a669e4e4bfa420dda1daf40b711d3
wy@manaosoftware.com <wy@manaosoftware.com>
2025-01-22 22:53:53 +0700

    init shadcn

--- hosting/src/app/not-found.tsx
+++ hosting/src/app/not-found.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-export default function Page() {
+import Link from 'next/link';
+
+export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404 555</h1>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <div className='bg-white p-8 rounded-lg shadow-lg text-center'>
+        <h1 className='text-6xl font-bold text-red-500'>404</h1>
+        <h2 className='text-2xl font-semibold mt-4'>Page Not Found</h2>
+        <p className='text-gray-600 mt-2'>
+          Sorry, the page you are looking for does not exist.
+        </p>
+        <Link
+          href='/'
+          className='mt-6 inline-block bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600 transition duration-300'
+        >
+          Go Back Home
+        </Link>
+      </div>
     </div>
   );
 }
