73ec18f05d5f82a14f834b9c31bf2a89f1275eb6
wy@manaosoftware.com <wy@manaosoftware.com>
2025-03-10 12:33:10 +0700

    update ci/cd

--- hosting/src/middleware.ts
+++ hosting/src/middleware.ts
@@ -4,7 +4,7 @@ const loginPath = '/admin/login';
 export async function middleware(req: NextRequest) {
   const { nextUrl } = req;
   const token = req.cookies.get('session_token')?.value;
-  console.warn('[nextUrl.pathname]', nextUrl.pathname, token);
+  console.warn('[nextUrl.pathname]', nextUrl.pathname);
 
   if (!token) {
     const url = req.nextUrl.clone();
