958c7f3f35dfa1bbc84b237dc101b5600b156dc8
wy@manaosoftware.com <wy@manaosoftware.com>
2025-05-06 17:55:57 +0700

    no message

--- hosting/src/app/(pages)/product/components/ProductItemDetail.tsx
+++ hosting/src/app/(pages)/product/components/ProductItemDetail.tsx
@@ -187,7 +187,7 @@ const ProductItemDetail: React.FC<ProductItemDetailProps> = ({
               <ul className='pl-4 mt-1'>
                 <li>
                   <a
-                    href='https://m.me/161993062306655'
+                    href='https://m.me/HmongBagspacharashopThailand'
                     target='_blank'
                     rel='noopener noreferrer'
                     className='text-blue-600 underline'
